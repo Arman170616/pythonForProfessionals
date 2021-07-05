3707b706e3719375fdfc94df27e4d1d334543a4e
Arman Hossain <64675035+Arman170616@users.noreply.github.com>
2021-07-05 06:40:17 +0600

    Create If_else_oneliner.py

---
+++ If_else_oneliner.py
@@ -0,0 +1,14 @@
+#gerneral
+
+a = 5
+b = 10
+if b > a:
+   print("b is greater than a")
+#output: b is greater than a
+ 
+  
+  
+#using one-liner 
+a,b = 5,10
+print("A") if a > b else print("B")
+#Output : B
